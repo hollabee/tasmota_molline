fc15e976d29a295286c9d91dd9f797e5241c4138
hollabee <37674881+hollabee@users.noreply.github.com>
2026-01-21 23:07:07 +0100

    Update user_config_override.h

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -1,105 +1,37 @@
 /*
   user_config_override.h - user configuration overrides my_user_config.h for Tasmota
-
-  Copyright (C) 2021  Theo Arends
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  Speziell angepasst für Molline Ultramess H / Diehl Sharky 775 mit M-Bus BCD Support
 */
 
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
-/*****************************************************************************************************\
- * USAGE:
- *   To modify the stock configuration without changing the my_user_config.h file:
- *   (1) copy this file to "user_config_override.h" (It will be ignored by Git)
- *   (2) define your own settings below
- *
- ******************************************************************************************************
- * ATTENTION:
- *   - Changes to SECTION1 PARAMETER defines will only override flash settings if you change define CFG_HOLDER.
- *   - Expect compiler warnings when no ifdef/undef/endif sequence is used.
- *   - You still need to update my_user_config.h for major define USE_MQTT_TLS.
- *   - All parameters can be persistent changed online using commands via MQTT, WebConsole or Serial.
-\*****************************************************************************************************/
-
-/*
-Examples :
-
-// -- Master parameter control --------------------
-#undef  CFG_HOLDER
-#define CFG_HOLDER        4617                   // [Reset 1] Change this value to load SECTION1 configuration parameters to flash
-
-// -- Setup your own Wifi settings  ---------------
-#undef  STA_SSID1
-#define STA_SSID1         "YourSSID"             // [Ssid1] Wifi SSID
-
-#undef  STA_PASS1
-#define STA_PASS1         "YourWifiPassword"     // [Password1] Wifi password
-
-// -- Setup your own MQTT settings  ---------------
-#undef  MQTT_HOST
-#define MQTT_HOST         "your-mqtt-server.com" // [MqttHost]
-
-#undef  MQTT_PORT
-#define MQTT_PORT         1883                   // [MqttPort] MQTT port (10123 on CloudMQTT)
-
-#undef  MQTT_USER
-#define MQTT_USER         "YourMqttUser"         // [MqttUser] Optional user
-
-#undef  MQTT_PASS
-#define MQTT_PASS         "YourMqttPass"         // [MqttPassword] Optional password
-
-// You might even pass some parameters from the command line ----------------------------
-// Ie:  export PLATFORMIO_BUILD_FLAGS='-DUSE_CONFIG_OVERRIDE -DMY_IP="192.168.1.99" -DMY_GW="192.168.1.1" -DMY_DNS="192.168.1.1"'
-
-#ifdef MY_IP
-#undef  WIFI_IP_ADDRESS
-#define WIFI_IP_ADDRESS     MY_IP                // Set to 0.0.0.0 for using DHCP or enter a static IP address
-#endif
-
-#ifdef MY_GW
-#undef  WIFI_GATEWAY
-#define WIFI_GATEWAY        MY_GW                // if not using DHCP set Gateway IP address
-#endif
-
-#ifdef MY_DNS
-#undef  WIFI_DNS
-#define WIFI_DNS            MY_DNS               // If not using DHCP set DNS IP address (might be equal to WIFI_GATEWAY)
-#endif
-
-#ifdef MY_DNS2
-#undef  WIFI_DNS2
-#define WIFI_DNS2           MY_DNS2              // If not using DHCP set DNS IP address (might be equal to WIFI_GATEWAY)
-#endif
-
-// !!! Remember that your changes GOES AT THE BOTTOM OF THIS FILE right before the last #endif !!!
-*/
-
+// --- Grundlegende Scripting Funktionen ---
 #ifndef USE_SCRIPT
 #define USE_SCRIPT
 #endif
+
+// --- Smart Meter Interface (SML) ---
 #ifndef USE_SML_M
 #define USE_SML_M
 #endif
+
+// --- M-Bus Protokoll & BCD Decoder (Essentiell für Sharky 775) ---
+#ifndef USE_SML_M_BUS
+#define USE_SML_M_BUS
+#endif
+
+// --- Regeln deaktivieren (spart Speicherplatz für Scripte) ---
 #ifdef USE_RULES
 #undef USE_RULES
 #endif
 
+// --- Automatisierungseinstellungen ---
 #define START_SCRIPT_FROM_BOOT
 #define USER_BACKLOG "script 1"
-#define PRECONFIGURED_SCRIPT ">D\n>B\n=>sensor53 r\n>M 1\n+1,3,s,0,9600,SMI,1\n1,77070100010800ff@1000,Total Consumed,kWh,Total_in,2\n1,77070100020800ff@1000,Total Delivered,kWh,Total_out,2\n1,77070100100700ff@1,Power,W,Power_cur,0\n#\n"
 
+// --- Standard-Script Platzhalter ---
+// Hinweis: Dieses Script wird durch dein manuelles Script im Webinterface ersetzt.
+#define PRECONFIGURED_SCRIPT ">D\n>B\n=>sensor53 r\n>M 1\n+1,3,s,0,9600,SMI,1\n1,77070100010800ff@1000,Total Consumed,kWh,Total_in,2\n1,77070100020800ff@1000,Total Delivered,kWh,Total_out,2\n1,77070100100700ff@1,Power,W,Power_cur,0\n#\n"
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
